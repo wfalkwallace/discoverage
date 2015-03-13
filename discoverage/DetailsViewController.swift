37b2c88c41797ef9b159a8a1119eeb70fafdaee1
Jehan <jehan.tremback@gmail.com>
2015-03-12 18:33:05 -0700

    Update DetailsViewController

--- discoverage/DetailsViewController.swift
+++ discoverage/DetailsViewController.swift
@@ -16,62 +16,62 @@ protocol DetailsViewControllerDelegate {
 class DetailsViewController: UIViewController {
 
     var user: User?
-    var monsterIndexRow : Int?
-    
+    var animalIndexRow : Int?
+
     var delegate: DetailsViewControllerDelegate?
     @IBOutlet weak var bananasCount: UILabel!
     @IBOutlet weak var navigationBar: UINavigationBar!
-    @IBOutlet weak var monsterImageView: UIImageView!
-    @IBOutlet weak var monsterName: UILabel!
+    @IBOutlet weak var animalImageView: UIImageView!
+    @IBOutlet weak var animalName: UILabel!
     @IBOutlet weak var healthMeter: UIProgressView!
     @IBOutlet weak var bananasEaten: UILabel!
-    @IBOutlet weak var monsterMood: UILabel!
+    @IBOutlet weak var animalMood: UILabel!
     @IBOutlet weak var feedButton: UIButton!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         var navigationItem = navigationBar.items[0] as! UINavigationItem
         var button = UIBarButtonItem(title: "Menagerie", style: .Bordered, target: self, action: "onBack")
         button.tintColor = UIColor.blackColor()
         navigationItem.leftBarButtonItem = button
-        let monster = user!.menagerie.animals[monsterIndexRow!]
-        navigationItem.title = monster["name"] as? String
+        let animal = user!.animals[animalIndexRow!]
+        navigationItem.title = animal.name as? String
 
         initView()
     }
-    
-    func initView() {
-        let animal = user!.menagerie.animals[monsterIndexRow!]
 
-        monsterImageView.image =  UIImage(named: animal["sprite"] as! String)
-        monsterName.text = animal["name"] as? String
-        
+    func initView() {
+        let animal = user?.animals[animalIndexRow!]
+
+        animalImageView.image =  UIImage(named: animal?.sprite as! String)
+        animalName.text = animal!.name as String
+
         UIProgressView.animateWithDuration(2.0, animations: {
-            self.healthMeter.setProgress(animal["health"] as! Float, animated: true)
+            self.healthMeter.setProgress(animal!.health as Float, animated: true)
         })
-        
+
         bananasCount.text = String(user!.bananaCount)
     }
-    
+
     func onBack() {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
 
     @IBAction func onFeed(sender: UIButton) {
-            
-        if (delegate?.healthShouldChange(monsterIndexRow!) == true) {
-            delegate?.healthDidChange(monsterIndexRow!)
+
+        if (delegate?.healthShouldChange(animalIndexRow!) == true) {
+            delegate?.healthDidChange(animalIndexRow!)
         }
-        
+
         //update this view
         bananasCount.text = String(user!.bananaCount)
-        
-        let animal = user!.menagerie.animals[monsterIndexRow!]
-        var health = animal["health"] as! Float
-        
+
+        let animal = user?.animals[animalIndexRow!]
+        var health = animal?.health as Float!
+
         UIProgressView.animateWithDuration(2.0, animations: {
-            self.healthMeter.setProgress(health,  animated: true)
+            self.healthMeter.setProgress(health!,  animated: true)
         })
     }
 }
