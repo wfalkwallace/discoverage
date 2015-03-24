2f5a0f7455c967f6c7c9489d06d2304e792f48b1
Jehan <jehan.tremback@gmail.com>
2015-03-23 20:32:57 -0700

    login works

--- discoverage/LoginViewController.swift
+++ discoverage/LoginViewController.swift
@@ -9,24 +9,51 @@
 import UIKit
 import Alamofire
 
-class LoginViewController: UIViewController {
+class RegisterViewController: UIViewController {
 
+
+    @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        loginButton.layer.borderWidth = 2
+        loginButton.layer.borderColor = UIColor.whiteColor().CGColor
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
-    @IBAction func didTapLogin(sender: UIButton) {
+    @IBAction func didTapSubmit(sender: AnyObject) {
+        register()
+    }
+    
+    func register () {
+        Alamofire.request(Discoverage.Router.UserCreate(["name": usernameTextField.text, "email" : emailTextField.text, "password": passwordTextField.text])).responseJSON { (_, _, data: AnyObject?, error: NSError?) -> Void in
+            if let error = error {
+                // deal with login error
+                let alert = UIAlertView()
+                alert.title = "Alert"
+                alert.message = "Signup failed. No network connection"
+                alert.addButtonWithTitle("OK")
+                alert.show()
+            } else if let data: AnyObject = data {
+                if let code: AnyObject = data.objectForKey("error") {
+                    println(data)
+                    let alert = UIAlertView()
+                    alert.title = "Alert"
+                    alert.message = data.objectForKey("error") as? String
+                    alert.addButtonWithTitle("OK")
+                    alert.show()
+                } else {
+                    self.login()
+                }
+            }
+        }
+    }
+    
+    func login () {
         Alamofire.request(Discoverage.Router.Login(emailTextField.text, passwordTextField.text))
             .responseJSON { (_, _, data: AnyObject?, error: NSError?) -> Void in
             if let error = error {
@@ -36,8 +63,8 @@ class LoginViewController: UIViewController {
                 alert.message = "Login failed. No network connection"
                 alert.addButtonWithTitle("OK")
                 alert.show()
-            } else if let data = data {
-                if let code = data.objectForKey("error") {
+            } else if let data: AnyObject = data {
+                if let code: AnyObject = data.objectForKey("error") {
                     println(data)
                     let alert = UIAlertView()
                     alert.title = "Alert"
@@ -54,18 +81,43 @@ class LoginViewController: UIViewController {
             }
         }
     }
+}
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     
-    @IBAction func didTapSignup(sender: UIButton) {
-        Alamofire.request(Discoverage.Router.UserCreate(["name": usernameTextField.text, "email" : emailTextField.text, "password": passwordTextField.text])).responseJSON { (_, _, data: AnyObject?, error: NSError?) -> Void in
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        registerButton.layer.borderWidth = 2
+        registerButton.layer.borderColor = UIColor.whiteColor().CGColor
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func didTapSubmit(sender: AnyObject) {
+        login()
+    }
+    
+    func login () {
+        Alamofire.request(Discoverage.Router.Login(emailTextField.text, passwordTextField.text))
+            .responseJSON { (_, _, data: AnyObject?, error: NSError?) -> Void in
             if let error = error {
                 // deal with login error
                 let alert = UIAlertView()
                 alert.title = "Alert"
-                alert.message = "Signup failed. No network connection"
+                alert.message = "Login failed. No network connection"
                 alert.addButtonWithTitle("OK")
                 alert.show()
-            } else if let data = data {
-                if let code = data.objectForKey("error") {
+            } else if let data: AnyObject = data {
+                if let code: AnyObject = data.objectForKey("error") {
                     println(data)
                     let alert = UIAlertView()
                     alert.title = "Alert"
@@ -73,11 +125,13 @@ class LoginViewController: UIViewController {
                     alert.addButtonWithTitle("OK")
                     alert.show()
                 } else {
-                    self.didTapLogin(sender)
+                    User.currentUser = User(dictionary: data as! NSDictionary)
+                    let menagerieStoryboard = UIStoryboard(name: "Menagerie", bundle: nil)
+                    let menagerieViewController = menagerieStoryboard.instantiateInitialViewController() as! UINavigationController
+                    self.presentViewController(menagerieViewController, animated: true, completion: nil)
                 }
             }
         }
     }
-
 }
 
