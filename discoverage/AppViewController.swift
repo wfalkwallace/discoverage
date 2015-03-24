3edf46186b21537c6a417017bff8eacfdb032311
William Falk-Wallace <wgf2104@columbia.edu>
2015-03-23 17:03:11 -0700

    add tabbar base controller and splash png

---
+++ discoverage/AppViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AppViewController.swift
+//  discoverage
+//
+//  Created by William Falk-Wallace on 3/23/15.
+//  Copyright (c) 2015 Discoverage. All rights reserved.
+//
+
+import UIKit
+
+class AppViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
